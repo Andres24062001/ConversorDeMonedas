c98682a80f2069b9ff176d57e319d30e0b42b587
Andres Manrique <andrsmmsa1@gmail.com>
2024-05-14 20:15:58 -0400

    cambios

---
+++ src/GeneradorDeArchivos.java
@@ -0,0 +1,33 @@
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class GeneradorDeArchivos {
+
+    public void guardarJson(Object jsonArray) throws IOException {
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+
+        FileWriter escritura = new FileWriter("HistorialDeConversiones.json");
+        escritura.write(gson.toJson(jsonArray));
+        escritura.close();
+    }
+
+    public void AgregarDatosAJsonGlobal(ArrayList<String> jsonArray) throws IOException {
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+
+        FileWriter escritura = new FileWriter("HistorialDeRegistros.json");
+        escritura.write(gson.toJson(jsonArray));
+        escritura.close();
+
+    }
+}
